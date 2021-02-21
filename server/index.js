072cc6ec6a739f9c6bf9e378796e17743d7eb874
ivangricenko <0.ivangricenko.0@gmail.com>
2021-02-21 16:21:18 +0300

    create server

---
+++ server/index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+require('dotenv').config();
+const PORT = process.env.PORT;
+const app = express();
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
